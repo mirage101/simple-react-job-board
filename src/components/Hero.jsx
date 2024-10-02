dbc886bdaccee25a9bb3b0e6c3977dcdcabdc694
ivan <ivan@stanga.net>
2024-10-02 11:55:55 +0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Hero({title, subtitle}) {
+  return (
+    <section className="py-20 mb-4 bg-indigo-700">
+      <div
+        className="flex flex-col items-center px-4 mx-auto max-w-7xl sm:px-6 lg:px-8"
+      >
+        <div className="text-center">
+          <h1
+            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
+          >
+            {title}
+          </h1>
+          <p className="my-4 text-xl text-white">
+            {subtitle}
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,10 @@
+import JobListings from '../components/JobListings';
+
+const JobsPage = () => {
+  return (
+    <section className="px-4 py-6 bg-blue-50">
+      <JobListings />
+    </section>
+  );
+};
+export default JobsPage;
